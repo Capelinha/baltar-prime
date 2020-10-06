40a9b912ede0ec4ddd9679b38e873c987d380e7f
Capelinha <mateus.igreja@letsenjoy.it>
2020-10-06 12:24:26 -0300

    Add winner page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ interface IPrime {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnInit  {
 
